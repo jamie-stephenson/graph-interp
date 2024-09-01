952d622df9311a98779644a204ebcee476b14be6
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-09-01 18:17:57 +0100

    Fix: not moving class token to correct device

--- models/adj.py
+++ models/adj.py
@@ -117,7 +117,7 @@ class AdjTransformer(HookedTransformer):
             if self.cfg.pool_type == "token":
                 # Add special class token to begining, our logits will be calculated from this position.
                 batch_size, _, n_vertices = input.shape
-                planar_token = torch.zeros((batch_size,1,n_vertices))
+                planar_token = torch.zeros((batch_size,1,n_vertices),device=self.cfg.device)
                 input = torch.cat([planar_token,input], dim=1)
 
             embed = self.hook_embed(self.embed(input))
