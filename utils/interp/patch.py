4d1ceccfac54e1f3609a832bbf4cc4f1ed17e2e5
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-09-24 22:30:30 +0100

    Patching PosEmbed

--- utils/interp/patch.py
+++ utils/interp/patch.py
@@ -22,12 +22,23 @@ def patch_head_acts(
     clean_cache: ActivationCache
 ) -> Float[Tensor, "batch n_vertices n_heads d_head"]:
     '''
-    Patches the activations (sepcified by `hook`) of a given head at
+    Hook function that patches the activations (sepcified by `hook`) of a given head at
     every vertex position, using the value from the clean cache.
     '''
     corrupted_head_outputs[:, :, head_ids] = clean_cache[hook.name][:, :, head_ids]
     
     return corrupted_head_outputs
+
+def patch_entire_act(
+    old_act: Tensor,
+    hook: HookPoint,
+    new_act: Tensor
+) -> Tensor:
+    '''
+    Hook function that patches the entire activation at `hook`.
+    Probably a more natural way to implement this using transformer_lens but will take me longer to find it than to write this.
+    '''
+    return new_act
 
 def run_with_patched_heads(
     model: Transformer,
@@ -41,7 +52,6 @@ def run_with_patched_heads(
         fwd_hooks=[('blocks.0.attn.hook_z',hook_fn)]
     )
 
-
 def get_path_patched_attn_pattern(
     model: Transformer,
     corrupted_input: Float[Tensor,"1 n_vertices n_vertices"],
@@ -53,8 +63,8 @@ def get_path_patched_attn_pattern(
     k_heads_to_patch: List[int],
     k_patch_embed: bool,
     layer_1_head: int
- ) -> Float[Tensor, "layer head"]:
-    """"
+ ) -> Float[Tensor, "n_vertices n_vertices"]:
+    """
     "Simultaneously" patch paths from two sets of components to the query and key of a layer 1 head.
     Return the resulting attention pattern from that head.
     """
@@ -108,6 +118,32 @@ def get_path_patched_attn_pattern(
 
     return pattern_cache['blocks.1.attn.hook_pattern'][:,layer_1_head].squeeze(0).detach()
 
+def get_pos_embed_to_key_path_patched_attn_pattern(
+    model: Transformer,
+    input: Float[Tensor,"batch n_vertices n_vertices"],
+    cache: ActivationCache,
+    new_pos_embed: Float[Tensor,"1 n_vertices d_model"],
+    layer_1_head: int
+ ) -> Float[Tensor, "n_vertices n_vertices"]:
+    
+    model.reset_hooks()
+
+    pos_embed_hook_fn = partial(patch_entire_act, new_act=new_pos_embed)
+    z_hook_fn = partial(patch_head_acts, head_ids=list(range(model.cfg.n_heads)), clean_cache=cache)
+    q_hook_fn = partial(patch_head_acts, head_ids=layer_1_head, clean_cache=cache)
+
+    pattern_cache = model.add_caching_hooks('blocks.1.attn.hook_pattern')
+
+    model.run_with_hooks(
+        input,
+        fwd_hooks=[
+            ('hook_pos_embed',pos_embed_hook_fn),
+            ('blocks.0.attn.hook_z',z_hook_fn),
+            ('blocks.1.attn.hook_q',q_hook_fn)
+        ]
+    )
+
+    return pattern_cache['blocks.1.attn.hook_pattern'][:,layer_1_head].squeeze(0).detach()
 
 def get_path_patching_metric_results(
     model: Transformer,
